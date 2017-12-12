8675dcaa93eb42099c2c83529b61aad3ee493e20
bekce <seb@sebworks.com>
2017-12-12 21:14:44 +0100

    Implement password change, separate profile functions

--- app/controllers/ProfileController.java
+++ app/controllers/ProfileController.java
@@ -1,65 +1,63 @@
 package controllers;
 
 import config.AuthorizationServerSecure;
-import dtos.ClientDto;
+import config.JwtUtils;
 import dtos.Utils;
-import models.Client;
 import models.User;
-import play.data.Form;
+import org.apache.commons.lang3.StringUtils;
 import play.data.FormFactory;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
-import repositories.ClientRepository;
+import repositories.UserRepository;
 
 import javax.inject.Inject;
-import java.util.List;
 
 public class ProfileController extends Controller {
 
     @Inject
-    private ClientRepository clientRepository;
-    @Inject
     private FormFactory formFactory;
+    @Inject
+    private UserRepository userRepository;
+    @Inject
+    private JwtUtils jwtUtils;
 
     @AuthorizationServerSecure
     public Result get() {
         User user = request().attrs().get(AuthorizationServerSecure.USER);
-        List<Client> clients = null;
-        if (user.isAdmin()) {
-            clients = clientRepository.findByOwnerId(user.getId());
-        }
-        return ok(views.html.profile.render(user, clients));
+        return ok(views.html.profile.render(user));
     }
 
-    @AuthorizationServerSecure(requireAdmin = true)
-    public Result addUpdateClient() {
+    @AuthorizationServerSecure
+    public Result changePassword(){
         User user = request().attrs().get(AuthorizationServerSecure.USER);
-        try {
-            Form<ClientDto> form = formFactory.form(ClientDto.class).bindFromRequest();
-            ClientDto dto = form.get();
-            if(dto.id == null){
-                Client client = new Client();
-                client.setId(Utils.newId());
-                client.setSecret(Utils.newId());
-                client.setOwnerId(user.getId());
-                client.setName(dto.name);
-                client.setRedirectUri(dto.redirectUri);
-                clientRepository.save(client);
-                flash("info", "Create client successful");
-            } else {
-                Client client = clientRepository.findById(dto.id);
-                if(!client.getOwnerId().equals(user.getId())){
-                    throw new IllegalAccessException();
-                }
-                client.setName(dto.name);
-                client.setRedirectUri(dto.redirectUri);
-                clientRepository.save(client);
-                flash("info", "Update successful");
-            }
-        } catch (Exception e) {
-            flash("error", e.getMessage());
+
+        String oldPassword = request().body().asFormUrlEncoded().get("oldPassword")[0];
+        String newPassword = request().body().asFormUrlEncoded().get("newPassword")[0];
+
+        if(StringUtils.isAnyBlank(oldPassword, newPassword)){
+            flash("error", "All fields are required");
+            return redirect(routes.ProfileController.get());
         }
-        return redirect(routes.ProfileController.get());
+        else if(!user.checkPassword(oldPassword)){
+            flash("error", "Current password is invalid");
+            return redirect(routes.ProfileController.get());
+        }
+        else if(Utils.newPasswordCheck(newPassword, newPassword) != null){
+            flash("error", Utils.newPasswordCheck(newPassword, newPassword));
+            return redirect(routes.ProfileController.get());
+        }
+        else {
+            user.encryptThenSetPassword(newPassword);
+            userRepository.save(user);
+            flash("success", "You have changed your password!");
+            // refresh the cookie here
+            String cookieValue = jwtUtils.prepareCookie(user);
+            Http.Cookie ltat = Http.Cookie.builder("ltat", cookieValue).withPath("/").withHttpOnly(true).withMaxAge(jwtUtils.getExpireCookie()).build();
+            return redirect(routes.ProfileController.get()).withCookies(ltat);
+        }
     }
+
+
 
 }
