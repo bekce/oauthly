1d1e5d89aadadcf9c6538baff90b2d74f7d31e76
bekce <seb@sebworks.com>
2018-01-13 01:43:58 +0100

    Recaptcha logger level change

--- app/config/RecaptchaAction.java
+++ app/config/RecaptchaAction.java
@@ -26,22 +26,22 @@ public class RecaptchaAction extends play.mvc.Action<RecaptchaProtected> {
         String recaptchaResponse = null;
         try {
             recaptchaResponse = ctx.request().body().asFormUrlEncoded().get("g-recaptcha-response")[0];
-            Logger.info("Found g-recaptcha-response by FormUrlEncoded");
+            Logger.debug("Found g-recaptcha-response by FormUrlEncoded");
             ctx.request().body().asFormUrlEncoded().remove("g-recaptcha-response");
         } catch (Exception ignored){}
         if(recaptchaResponse == null) {
             try {
                 recaptchaResponse = ctx.request().body().asMultipartFormData().asFormUrlEncoded().get("g-recaptcha-response")[0];
-                Logger.info("Found g-recaptcha-response by MultipartFormData");
+                Logger.debug("Found g-recaptcha-response by MultipartFormData");
                 ctx.request().body().asMultipartFormData().asFormUrlEncoded().remove("g-recaptcha-response");
             } catch (Exception ignored){}
         }
         return recaptchaUtil.check(clientIp, recaptchaResponse).thenComposeAsync(passed -> {
             if(passed) {
-                Logger.info("Recaptcha passes");
+                Logger.debug("Recaptcha passes");
                 return delegate.call(ctx);
             } else {
-                Logger.info("Recaptcha didn't pass");
+                Logger.debug("Recaptcha didn't pass");
                 ctx.flash().put("error", "Your request did not succeed - captcha required");
                 return CompletableFuture.completedFuture(redirect(
                         configuration.fallback().isEmpty() ? ctx._requestHeader().asJava().path() : configuration.fallback()
